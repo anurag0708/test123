26a1ff10c9b730e2a1b0d6588cd7cbef9229e170
Anurag Pandey <Anurag.Pandey@synechron.com>
2019-08-14 17:03:37 +0530

    Changes for soap producer

---
+++ src/main/java/com/practice/soapproducer/SoapproducerApplication.java
@@ -0,0 +1,13 @@
+package com.practice.soapproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapproducerApplication.class, args);
+	}
+
+}
